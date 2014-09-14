fb9e22aba550ebce4e4f19c9fbaec9d55e39e217
Kerry Jiang <kerry-jiang@hotmail.com>
2014-09-14 14:11:38 -0700

    made it can run

--- MasterServer/LogMasterServer.cs
+++ MasterServer/LogMasterServer.cs
@@ -1,5 +1,7 @@
 ﻿using SuperSocket.ProtoBase;
 using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Logging;
 using SuperSocket.SocketBase.Protocol;
 using System;
 using System.Collections.Generic;
@@ -11,22 +13,36 @@ namespace LogMaster4Net.MasterServer
 {
     public class LogMasterServer : AppServer<AppSession>
     {
+        private ILog m_LogMasterLog;
+
         public LogMasterServer()
             : base(new DefaultReceiveFilterFactory<LogReceiveFilter, StringPackageInfo>())
         {
             this.NewRequestReceived += LogMasterServer_NewRequestReceived;
         }
 
+        protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
+        {
+            m_LogMasterLog = LogFactory.GetLog("LogMaster");
+            return base.Setup(rootConfig, config);
+        }
+
         private Timer m_Timer;
 
         protected override void OnStarted()
         {
-            m_Timer = new Timer(OnTimerCallback, null, 5000, 5000);
+            m_Timer = new Timer(OnTimerCallback, null, 1000 * 60, 1000 * 60);
         }
 
         private void OnTimerCallback(object state)
         {
-            Logger.InfoFormat("Current time: {0}", DateTime.Now);
+            m_LogMasterLog.InfoFormat("Current time: {0}", DateTime.Now);
+        }
+
+        protected override void OnStopped()
+        {
+            m_Timer.Dispose();
+            m_Timer = null;
         }
 
         void LogMasterServer_NewRequestReceived(AppSession session, StringPackageInfo requestInfo)

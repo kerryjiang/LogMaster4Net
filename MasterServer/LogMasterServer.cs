45e5e2483f17189cb4f6d9e4824c136c79c6d8ef
Kerry Jiang <kerry-jiang@hotmail.com>
2014-09-14 12:23:07 -0700

    commit main structure

---
+++ MasterServer/LogMasterServer.cs
@@ -0,0 +1,37 @@
+﻿using SuperSocket.ProtoBase;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LogMaster4Net.MasterServer
+{
+    public class LogMasterServer : AppServer<AppSession>
+    {
+        public LogMasterServer()
+            : base(new DefaultReceiveFilterFactory<LogReceiveFilter, StringPackageInfo>())
+        {
+            this.NewRequestReceived += LogMasterServer_NewRequestReceived;
+        }
+
+        private Timer m_Timer;
+
+        protected override void OnStarted()
+        {
+            m_Timer = new Timer(OnTimerCallback, null, 5000, 5000);
+        }
+
+        private void OnTimerCallback(object state)
+        {
+            Logger.InfoFormat("Current time: {0}", DateTime.Now);
+        }
+
+        void LogMasterServer_NewRequestReceived(AppSession session, StringPackageInfo requestInfo)
+        {
+            Console.WriteLine(requestInfo.Body);
+        }
+    }
+}
---
+++ MasterServer/LogReceiveFilter.cs
@@ -0,0 +1,30 @@
+﻿using SuperSocket.ProtoBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogMaster4Net.MasterServer
+{
+    public class LogReceiveFilter : IReceiveFilter<StringPackageInfo>
+    {
+        public StringPackageInfo Filter(BufferList data, out int rest)
+        {
+            rest = 0;
+
+            using (var reader = this.GetBufferReader(data))
+            {
+                return new StringPackageInfo(string.Empty, reader.ReadString(data.Total, Encoding.UTF8), null);
+            }
+        }
+
+        public IReceiveFilter<StringPackageInfo> NextReceiveFilter { get; private set; }
+
+        public void Reset()
+        {
+
+        }
+
+        public FilterState State { get; private set; }
+    }
+}

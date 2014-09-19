695dd4117a44293fb9cc2a119256029f0aadb214
Kerry Jiang <kerry-jiang@hotmail.com>
2014-09-19 10:15:18 -0700

    updated SuperSocket and removed some test code

--- MasterServer/LogMasterServer.cs
+++ MasterServer/LogMasterServer.cs
@@ -16,8 +16,6 @@ namespace LogMaster4Net.MasterServer
 {
     public class LogMasterServer : AppServer<LoggingSession, LoggingPackageInfo>
     {
-        private ILog m_LogMasterLog;
-
         private ILoggingDeserializer m_LoggingDeserializer;
 
         internal ILoggingDeserializer LoggingDeserializer
@@ -36,27 +34,18 @@ namespace LogMaster4Net.MasterServer
 
         protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
         {
-            m_LogMasterLog = LogFactory.GetLog("LogMaster");
             m_LoggingDeserializer = new Log4NetLoggingDeserializer();
             return base.Setup(rootConfig, config);
         }
 
-        private Timer m_Timer;
-
         protected override void OnStarted()
         {
-            m_Timer = new Timer(OnTimerCallback, null, 1000 * 60, 1000 * 60);
-        }
 
-        private void OnTimerCallback(object state)
-        {
-            m_LogMasterLog.InfoFormat("Current time: {0}", DateTime.Now);
         }
 
         protected override void OnStopped()
         {
-            m_Timer.Dispose();
-            m_Timer = null;
+
         }
 
         void LogMasterServer_NewRequestReceived(LoggingSession session, LoggingPackageInfo requestInfo)

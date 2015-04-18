889f16b22ba5313cb1aad25e1a5b1d847b038036
Kerry Jiang <kerry-jiang@hotmail.com>
2015-04-18 23:35:00 +0800

    added global assembly info

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("LogMaster4Net")]
+[assembly: AssemblyProduct("LogMaster4Net")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

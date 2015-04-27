a4a44337ef7217b4c44e3ada76cf869b83da4b0e
Kerry Jiang <kerry-jiang@hotmail.com>
2015-04-27 13:54:41 +0800

    added project for NLog

---
+++ NLogAdapter/NLogLoggingDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using LogMaster4Net.Base;
+using SuperSocket.SocketBase.Logging;
+
+namespace LogMaster4Net.NLogAdapter
+{
+    [Export(typeof(ILoggingDeserializer))]
+    [ExportMetadata("Name", "NLog")]
+    public class NLogLoggingDeserializer : ILoggingDeserializer
+    {
+        public IList<LoggingData> Deserialize(string log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NLogAdapter/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogMaster4Net.NLogAdapter")]
+[assembly: AssemblyDescription("LogMaster4Net.NLogAdapter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d527fbea-c7d9-4fdd-aa3a-900920e452ff")]

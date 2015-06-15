90b87903184504008977060faa959f722da0be52
Kerry Jiang <kerry-jiang@hotmail.com>
2015-06-15 11:19:18 +0800

    upgraded version number to 0.2

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

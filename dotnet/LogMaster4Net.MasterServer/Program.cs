25568f1b5797e9692ca27c3d9345f9c8c03e2501
Kerry Jiang <kerry-jiang@hotmail.com>
2016-12-09 23:15:57 -0800

    added the MasterServer project for .net core

---
+++ dotnet/LogMaster4Net.MasterServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace LogMaster4Net.MasterServer
+{
+    public class Program
+    {
+        private static ILogger _logger;
+
+        public static void Main(string[] args)
+        {
+            var factory = new LoggerFactory();
+            factory.AddConsole();
+            _logger = factory.CreateLogger("MasterServer");
+            _logger.LogInformation("Hello World!");
+        }
+    }
+}
